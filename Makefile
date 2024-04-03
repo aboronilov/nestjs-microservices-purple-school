2f969ae8cf714dac4826ecb7920a6d932e50e121
Anatoly Boronilov <boronilov@gmail.com>
2024-04-03 10:49:21 +0200

    auth contracts

---
+++ Makefile
@@ -0,0 +1,7 @@
+include envs/.account.env
+
+create_db:
+	docker run --name purple-mongo -p ${MONGO_PORT}:${MONGO_PORT} -e MONGO_INITDB_ROOT_USERNAME=${MONGO_LOGIN} -e MONGO_INITDB_ROOT_PASSWORD=${MONGO_LOGIN} -d mongo:7.0
+
+create_rabbitmq:
+	docker run --name purple-rabbit -p 15672:15672 -p 5672:5672 -d rabbitmq:3.13.1-management-alpine
--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,30 +1,24 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
-export class RegisterDto {
-  email: string;
-  password: string;
-  displayName?: string;
-}
-
-export class LoginDto {
-  email: string;
-  password: string;
-}
+import { AccountLogin, AccountRegister } from '@purple/contracts';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() registerDto: RegisterDto) {
+  async register(
+    @Body() registerDto: AccountRegister.Request
+  ): Promise<AccountRegister.Response> {
     return this.authService.register(registerDto);
   }
 
   @Post('login')
-  async login(@Body() { email, password }: LoginDto) {
+  async login(
+    @Body() { email, password }: AccountLogin.Request
+  ): Promise<AccountLogin.Response> {
     const { id } = await this.authService.validateUser(email, password);
 
-    return this.authService.login(id)
+    return this.authService.login(id);
   }
 }
